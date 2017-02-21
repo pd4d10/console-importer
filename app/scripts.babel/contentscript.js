bf7d3b65d6d8eb5c7b7d0522f72f1d329e566fef
pd4d10 <pd4d10@gmail.com>
2017-02-21 19:44:12 +0800

    Add tips when name not match, or no result found

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,17 +3,25 @@ console.log('\'Allo \'Allo! Content script');
 function inject(name) {
   const CDN_JS = 'https://api.cdnjs.com/libraries?search='
 
-  return fetch(`${CDN_JS}/${name}`)
+  fetch(`${CDN_JS}/${name}`)
     .then(res => res.json())
     .then(({ results }) => {
-      const scriptSrc = results[0].latest
+      if (results.length === 0) {
+        console.log(`Sorry, ${name} not found`)
+      }
+
+      const { name: exactName, latest: scriptSrc } = results[0]
+
+      if (name !== exactName) {
+        console.log(`${name} not found, inject ${exactName} for you.`)
+      }
 
       const script = document.createElement('script')
       script.src = scriptSrc
-      script.onload = () => console.log(`${name} loaded`)
+      script.onload = () => console.log(`${exactName} loaded`)
 
       document.body.appendChild(script)
-      console.log(`${name} loading...`)
+      console.log(`${exactName} loading...`)
     })
 }
 
@@ -24,7 +32,5 @@ function createScript(content, onload) {
 
   document.body.appendChild(script)
 }
-
-// window.$i = inject
 
 createScript(`window.$i = ${inject.toString()}`, console.log)
--- test.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('input')
-
-input.addEventListener('keyup', function () {
-  fetch('https://api.cdnjs.com/libraries?search=' + input.value)
-    .then(res => res.json())
-    .then(json => {
-      document.querySelector('table').innerHTML = json
-    })
-})
