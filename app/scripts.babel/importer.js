d91a743ae0d1204ca5be8c78f2681e7c0e5a4fe5
pd4d10 <pd4d10@gmail.com>
2017-07-21 22:39:06 +0800

    Use Tiza to do console styling

--- app/scripts.babel/importer.js
+++ app/scripts.babel/importer.js
@@ -1,11 +1,11 @@
-import cholk from 'cholk'
+import tiza from 'tiza'
 
 const PREFIX_TEXT = '[$i]: '
-const prefix = cholk.blue
-const strong = cholk.blue.bold
-const error = cholk.red
-const log = (...args) => cholk.log(prefix(PREFIX_TEXT), ...args)
-const logError = (...args) => cholk.log(error(PREFIX_TEXT), ...args)
+const prefix = tiza().color('blue').text
+const strong = tiza().color('blue').bold().text
+const error = tiza().color('red').text
+const log = (...args) => tiza().log(prefix(PREFIX_TEXT), ...args)
+const logError = (...args) => tiza().log(error(PREFIX_TEXT), ...args)
 
 function createBeforeLoad(name) {
   return () => log(strong(name), ' is loading, please be patient...')
