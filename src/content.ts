a84f250966ba87cdff1aa08fbc368e7160464896
Rongjian Zhang <pd4d10@gmail.com>
2022-12-26 01:07:25 +0800

    fix: match content script config

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,13 @@
 // https://stackoverflow.com/questions/20499994/access-window-variable-from-content-script
+import type { PlasmoContentScript } from 'plasmo'
 import importer from 'url:./importer.ts'
+
+export const config: PlasmoContentScript = {
+  // matches: ['<all_urls>'],
+  all_frames: true,
+  match_about_blank: true,
+  run_at: 'document_end',
+}
 
 const script = document.createElement('script')
 script.src = importer
